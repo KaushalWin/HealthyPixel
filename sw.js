f45b93121fcaac577d37e5bc6d4b55a8f29ff861
KaushalWin <KaushalWin@users.noreply.github.com>
2026-04-05 13:32:25 +0000

    deploy: 7827df900785d253d49bad3dfbf8d36776ff4755

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'healthy-pixel-shell-v1';
+const APP_SHELL = [
+  '/healthy-pixel/',
+  '/healthy-pixel/index.html',
+  '/healthy-pixel/manifest.webmanifest',
+  '/healthy-pixel/favicon.svg',
+  '/healthy-pixel/icon-192.png',
+  '/healthy-pixel/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request)
+        .then((response) => {
+          const cloned = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
+          return response;
+        })
+        .catch(() => caches.match('/healthy-pixel/index.html'));
+    })
+  );
+});
